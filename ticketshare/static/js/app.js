1294880eae43ab94cd8bc4fe95c3f9da06d73606
Johanna <jweinsziehr@gmx.de>
2014-12-09 22:31:04 +0100

    statics

--- ticketshare/static/js/app.js
+++ ticketshare/static/js/app.js
@@ -9,8 +9,8 @@ ticket_ang.run(function ($http, $cookies) {
 
 ticket_ang.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-        when('/', {templateUrl: '/static/js/views/home.html', controller: homeController }).
-        when('/journey/:id', {templateUrl: '/static/js/views/journey.html', controller: journeyController }).
-        when('/user/:id', {templateUrl: '/static/js/views/user.html', controller: userController }).
+        when('/', {templateUrl: '/ticket/static/js/views/home.html', controller: homeController }).
+        when('/journey/:id', {templateUrl: '/ticket/static/js/views/journey.html', controller: journeyController }).
+        when('/user/:id', {templateUrl: '/ticket/static/js/views/user.html', controller: userController }).
         otherwise({redirectTo: '/'});
 }]);
