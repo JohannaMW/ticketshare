1f7a424dbf48f576cf6ddb1ec73e5304ed6b2f50
Johanna <jweinsziehr@gmx.de>
2014-12-09 21:30:03 +0100

    heroku

--- ticket/wsgi.py
+++ ticket/wsgi.py
@@ -9,10 +9,13 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-application = Cling(get_wsgi_application())
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ticket.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
