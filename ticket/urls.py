d6468ae94a4f9c9f417450c6a8e4dc19a55c66ec
Johanna <jweinsziehr@gmx.de>
2014-12-11 14:49:51 +0100

    change logout redirect

--- ticket/urls.py
+++ ticket/urls.py
@@ -20,7 +20,7 @@ urlpatterns = patterns('',
     # User Authentification
     url(r'^register/$', 'ticketshare.views.register', name='register'),
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': 'home'}, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
 
     #REST
